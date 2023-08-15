d2654b96a41dacd538288b3fc9cf537f5401f3b3
vivek <vivekshinde775@gmail.com>
2023-08-15 22:54:27 +0530

    mongodb connection string

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,10 @@
+// module.exports = {
+//     url: "mongodb://127.0.0.1:27017/blog_db"
+//   };
+
+
 module.exports = {
-    url: "mongodb://127.0.0.1:27017/blog_db"
+    url: "mongodb+srv://vivekshinde775:guOa9Rs6GFC0MLIQ@cluster0blog.vyt1wnu.mongodb.net/?retryWrites=true&w=majority/blog_db"
   };
+
+
